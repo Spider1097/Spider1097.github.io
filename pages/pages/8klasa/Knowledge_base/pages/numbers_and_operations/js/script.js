1e0518010bd231950f7b9676efb9acdbc7910a80
A_H <118929720+Spider1097@users.noreply.github.com>
2026-07-21 00:02:38 +0200

    f

--- pages/pages/8klasa/Knowledge_base/pages/numbers_and_operations/js/script.js
+++ pages/pages/8klasa/Knowledge_base/pages/numbers_and_operations/js/script.js
@@ -30,7 +30,59 @@ const elementMap = {
     examplesHeader_2_1: "examplesHeader_2_1",
     exampleText_2_1: "exampleText_2_1",
     exampleText_2_2: "exampleText_2_2",
-    exampleText_2_3: "exampleText_2_3"
+    exampleText_2_3: "exampleText_2_3",
+
+    section_title_3: "section_title_3",
+    explanation_text_3_1: "explanation_text_3_1",
+    explanation_text_3_2: "explanation_text_3_2",
+    explanation_text_3_3: "explanation_text_3_3",
+    rulesHeader_3_1: "rulesHeader_3_1",
+    ruleText_3_1: "ruleText_3_1",
+    ruleText_3_2: "ruleText_3_2",
+    ruleText_3_3: "ruleText_3_3",
+    examplesHeader_3_1: "examplesHeader_3_1",
+    exampleText_3_1: "exampleText_3_1",
+    exampleText_3_2: "exampleText_3_2",
+    exampleText_3_3: "exampleText_3_3",
+    exampleText_3_4: "exampleText_3_4",
+
+    section_title_4: "section_title_4",
+    explanation_text_4_1: "explanation_text_4_1",
+    explanation_text_4_2: "explanation_text_4_2",
+    explanation_text_4_3: "explanation_text_4_3",
+    rulesHeader_4_1: "rulesHeader_4_1",
+    ruleText_4_1: "ruleText_4_1",
+    ruleText_4_2: "ruleText_4_2",
+    ruleText_4_3: "ruleText_4_3",
+    ruleText_4_4: "ruleText_4_4",
+    examplesHeader_4_1: "examplesHeader_4_1",
+    exampleText_4_1: "exampleText_4_1",
+    exampleText_4_2: "exampleText_4_2",
+    exampleText_4_3: "exampleText_4_3",
+    exampleText_4_4: "exampleText_4_4",
+
+    section_title_5: "section_title_5",
+    explanation_text_5_1: "explanation_text_5_1",
+    explanation_text_5_2: "explanation_text_5_2",
+    explanation_text_5_3: "explanation_text_5_3",
+
+    formulasHeader_5_1: "formulasHeader_5_1",
+    formulasHeader_5_2: "formulasHeader_5_2",
+    formulasHeader_5_3: "formulasHeader_5_3",
+
+    rulesHeader_5_1: "rulesHeader_5_1",
+    ruleText_5_1: "ruleText_5_1",
+    ruleText_5_2: "ruleText_5_2",
+    ruleText_5_3: "ruleText_5_3",
+    ruleText_5_4: "ruleText_5_4",
+    examplesHeader_5_1: "examplesHeader_5_1",
+    exampleText_5_1: "exampleText_5_1",
+    exampleText_5_2: "exampleText_5_2",
+    exampleText_5_3: "exampleText_5_3",
+    exampleText_5_4: "exampleText_5_4",
+
+    contactTitle: "contactTitle"
+
 };
 
 function filterCards() {
